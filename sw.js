eeb779da0e6ebbed2e38cc166d3eedd066f36189
Rafael <feliz.metaleiro@gmail.com>
2026-02-11 22:29:05 -0300

    Update sw.js to offline access

--- sw.js
+++ sw.js
@@ -1,4 +1,23 @@
-self.addEventListener('fetch', function(event) {
-  // Isso permite que o site funcione de forma básica
-  event.respondWith(fetch(event.request));
+const cacheName = 'medcti-v1';
+const resourcesToPrecache = [
+  './',
+  'index.html' // Se o seu arquivo principal tiver outro nome, troque aqui
+];
+
+// Instala e guarda os arquivos no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(resourcesToPrecache);
+    })
+  );
+});
+
+// Faz o site carregar do cache se estiver sem internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
